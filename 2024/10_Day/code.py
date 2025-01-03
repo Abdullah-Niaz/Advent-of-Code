957313c34b70a6ece6c2927de50e82b7f2bc9dc3
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-01-03 13:39:04 +0500

    Day 10 Solution

---
+++ 2024/10_Day/code.py
@@ -0,0 +1,70 @@
+
+
+class Solution():
+    def get_score(self, _map, i, j):
+        rows, cols = len(_map), len(_map[0])
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        endpoints = []
+        queue = [(i, j)]
+
+        while queue:
+            y, x = queue.pop(0)
+            curr = _map[y][x]
+            _next = curr + 1
+            for dy, dx in dirs:
+                ny, nx = y + dy, x + dx
+                if 0 <= ny < rows and 0 <= nx < cols and _map[ny][nx] == _next:
+                    if _next == 9:
+                        endpoints.append((ny, nx))
+                    else:
+                        queue.append((ny, nx))
+
+        return endpoints
+
+    def part1(self, data):
+        _map = [list(map(int, line)) for line in data]
+        rows, cols = len(_map), len(_map[0])
+
+        score = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if _map[i][j] == 0:
+                    endpoints = self.get_score(_map, i, j)
+                    score += len(set(endpoints))
+        return score
+
+    def part2(self, data):
+        _map = [list(map(int, line)) for line in data]
+        rows, cols = len(_map), len(_map[0])
+
+        score = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if _map[i][j] == 0:
+                    endpoints = self.get_score(_map, i, j)
+                    score += len(endpoints)
+        return score
+
+
+def main():
+    # Reading input data from 'input.txt' file
+    with open('input.txt', 'r') as file:
+        data = file.read().strip().splitlines()
+
+    # Create an instance of the Solution class
+    solution = Solution()
+
+    # Calling part1 and part2 methods with the input data
+    part1_result = solution.part1(data)
+    part2_result = solution.part2(data)
+
+    # Printing the results for both parts
+    print(f"Part 1 Result: {part1_result}")
+    print(f"Part 2 Result: {part2_result}")
+
+
+if __name__ == "__main__":
+    main()
